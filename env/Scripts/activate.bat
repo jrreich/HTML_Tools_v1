63fca7f80ed409102ba115877162292691a5e92c
Jesse Reich <jrreich@gmail.com>
2017-01-03 00:39:24 -0500

    test mod to index3

--- env/Scripts/activate.bat
+++ env/Scripts/activate.bat
@@ -1,5 +1,5 @@
 @echo off
-set "VIRTUAL_ENV=C:\Users\Jesse\Documents\Programming\Flask\FlaskWebProject1\env"
+set "VIRTUAL_ENV=C:\Users\reichj\Source\Repos\HTML_Tools_v1\env"
 
 if defined _OLD_VIRTUAL_PROMPT (
     set "PROMPT=%_OLD_VIRTUAL_PROMPT%"
